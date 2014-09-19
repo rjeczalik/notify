190ea6da1ad182365fc8a07985f50646c3a82315
Pawel Knap <pawelknap88@gmail.com>
2014-09-19 23:08:15 +0200

    Life is not easy - rework Event type

--- notify.go
+++ notify.go
@@ -1,5 +1,36 @@
 package notify
 
+import "strings"
+
+type Event int
+
+// TODO(someone) : what if invalid value is casted to Event type?
+func (e Event) String() string {
+	var z []string
+	for _, event := range splitevents(e) {
+		z = append(z, events[int(event)])
+	}
+	return strings.Join(z, ",")
+}
+
+// TODO(someone): splitevents supports only generic events.
+func splitevents(e Event) (events []Event) {
+	for _, event := range []Event{Create, Delete, Write, Move, Recursive} {
+		if e&event != 0 {
+			events = append(events, event)
+		}
+	}
+	return
+}
+
+// EventInfo TODO
+type EventInfo interface {
+	Event() Event     // TODO
+	IsDir() bool      // TODO
+	Name() string     // TODO
+	Sys() interface{} // TODO
+}
+
 // Watch TODO
 func Watch(name string, c chan<- EventInfo, events ...Event) {
 	global.Watch(name, c, events...)
