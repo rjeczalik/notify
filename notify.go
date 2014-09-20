8c73f6fbc9614d6378f63f79acce8f420637c3d4
Pawel Knap <pawelknap88@gmail.com>
2014-09-20 22:50:15 +0200

    Changes done after rebase

--- notify.go
+++ notify.go
@@ -1,52 +1,5 @@
 package notify
 
-import (
-	"strconv"
-	"strings"
-)
-
-type Event int
-
-// Kind TODO
-func (e Event) Kind() Event {
-	if ek, ok := evkinds[int(e)]; ok {
-		return ek
-	}
-	panic("notify: invalid event type - " + e.String())
-}
-
-// String TODO
-func (e Event) String() string {
-	var z []string
-	for _, event := range splitevents(e) {
-		if en := evnames[int(event)]; en != "" {
-			z = append(z, en)
-		} else {
-			z = append(z, "event "+strconv.Itoa(int(event)))
-		}
-
-	}
-	return strings.Join(z, ",")
-}
-
-// TODO(someone): splitevents supports only generic events.
-func splitevents(e Event) (events []Event) {
-	for _, event := range []Event{Create, Delete, Write, Move} {
-		if e&event != 0 {
-			events = append(events, event)
-		}
-	}
-	return
-}
-
-// EventInfo TODO
-type EventInfo interface {
-	Event() Event     // TODO
-	IsDir() bool      // TODO
-	Name() string     // TODO
-	Sys() interface{} // TODO
-}
-
 // Watch TODO
 func Watch(name string, c chan<- EventInfo, events ...Event) {
 	global.Watch(name, c, events...)
