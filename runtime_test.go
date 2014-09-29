efe7102432f3d147f5d34528682c32b2814e4222
Rafal Jeczalik <rjeczalik@gmail.com>
2014-09-29 12:47:35 +0200

    Add stub for Runtime test

--- runtime_test.go
+++ runtime_test.go
@@ -1,7 +1,21 @@
 package notify
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestRuntime(t *testing.T) {
-	t.Skip("TODO(rjeczalik)")
+	spy, c, cas := &Spy{}, make(chan EventInfo, 1), fixture.Cases(t)
+	r, p := newRuntime(spy), cas.path("github.com/rjeczalik/fakerpc")
+	calls := &Spy{
+		{T: TypeFanin},
+		{T: TypeWatch, P: p, E: Delete},
+	}
+	if err := r.Watch(p, c, Delete); err != nil {
+		t.Fatalf("want err=nil; got %v", err)
+	}
+	if !reflect.DeepEqual(spy, calls) {
+		t.Errorf("want spy=%+v; got %+v", calls, spy)
+	}
 }
