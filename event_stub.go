5e46a68ce5aa6c61a664d327ed712c9dc98cf71c
Rafal Jeczalik <rjeczalik@gmail.com>
2014-11-02 08:51:55 +0100

    Add Recursive internal event to stubs

--- event_stub.go
+++ event_stub.go
@@ -4,19 +4,21 @@
 package notify
 
 const (
-	Create = EV_STUB_CREATE
-	Delete = EV_STUB_DELETE
-	Write  = EV_STUB_WRITE
-	Move   = EV_STUB_MOVE
+	Create    = EV_STUB_CREATE
+	Delete    = EV_STUB_DELETE
+	Write     = EV_STUB_WRITE
+	Move      = EV_STUB_MOVE
+	Recursive = EV_STUB_RECURSIVE // internal event
 )
 
 // Events TODO
 const (
-	EV_STUB_CREATE = Event(0x00000001)
-	EV_STUB_DELETE = Event(0x00000002)
-	EV_STUB_WRITE  = Event(0x00000004)
-	EV_STUB_MOVE   = Event(0x00000008)
-	EV_STUB_ALL    = Event(0x0000000f)
+	EV_STUB_CREATE    = Event(0x01)
+	EV_STUB_DELETE    = Event(0x02)
+	EV_STUB_WRITE     = Event(0x04)
+	EV_STUB_MOVE      = Event(0x08)
+	EV_STUB_RECURSIVE = Event(0x0f)
+	EV_STUB_ALL       = Event(0x1f)
 )
 
 var osestr = map[Event]string{}
