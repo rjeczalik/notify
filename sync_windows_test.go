88c06a8739684f3ccce05741d6e6f3df9488806e
Rafal Jeczalik <rjeczalik@gmail.com>
2014-12-29 12:41:58 +0100

    fix underflow for SetSystemFileCacheSize

--- sync_windows_test.go
+++ sync_windows_test.go
@@ -5,14 +5,18 @@ package notify
 // TODO(rjeczalik): this repo works only go1.4
 // import "golang.org/x/sys/windows"
 
-import "syscall"
+import (
+	"syscall"
+	"unsafe"
+)
 
 var modkernel32 = syscall.NewLazyDLL("kernel32.dll")
 var procSetSystemFileCacheSize = modkernel32.NewProc("SetSystemFileCacheSize")
+var none = uintptr(1<<(unsafe.Sizeof(uintptr(0))*8) - 1)
 
 // TODO
 func Sync() {
-	r, _, err := procSetSystemFileCacheSize.Call(-1, -1, 0)
+	r, _, err := procSetSystemFileCacheSize.Call(none, none, 0)
 	if r == 0 {
 		panic(err)
 	}
