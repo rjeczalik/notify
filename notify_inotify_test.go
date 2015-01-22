431d451ef4363cd828810b6742bb37d0023660ba
Pawel Knap <pawelknap88@gmail.com>
2015-01-22 19:50:04 +0100

    linux : system event vs notify event on one folder - test

---
+++ notify_inotify_test.go
@@ -0,0 +1,25 @@
+// +build linux
+
+package notify
+
+import "testing"
+
+func TestNotifySystemAndGlobalMix(t *testing.T) {
+	t.Skip("TODO(ppknap)")
+	n := NewNotifyTest(t, "testdata/vfs.txt")
+	defer n.Close()
+
+	ch := NewChans(2)
+
+	n.Watch("src/github.com/rjeczalik/fs", ch[0], Create)
+	n.Watch("src/github.com/rjeczalik/fs", ch[1], IN_CREATE)
+
+	cases := []NCase{
+		{
+			Event:    create(n.W(), "src/github.com/rjeczalik/fs/.main.cc.swr"),
+			Receiver: Chans{ch[0], ch[1]},
+		},
+	}
+
+	n.ExpectNotifyEvents(cases, ch)
+}
