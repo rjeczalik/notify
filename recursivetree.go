rjeczalik/notify

commit 524da74166d489f452548227a53937c3d9ab7b82

skip traversing during Stop only if watchpoint is empty
Fixes a case when Stop on chan that does not affect overall state of
watchpoint causes children watchpoints not being deleted.

recursivetree.go
@@ -107,8 +107,24 @@ func watchTotal(nd node) Event {
 // watchIsRecursive TODO(rjeczalik)
 func watchIsRecursive(nd node) bool {
 	ok := nd.Watch.IsRecursive()
+	// TODO(rjeczalik): add a test for len(wp) != 0 change the condition.
 	if wp := nd.Child[""].Watch; len(wp) != 0 {
 		ok = ok || wp.IsRecursive()
+		// If a watchpoint holds inactive watchpoints, it means it's a parent
+		// one, which is recursive by nature even though it may be not recursive
+		// itself. It currently does not work due to some watchpoints being
+		// registered twice - one for actual watchpoint and one for an inactive
+		// one.
+		//
+		//   ok = true
+		//
+		// TODO(rjeczalik): fix this hack:
+		for c, _ := range wp {
+			if _, b := nd.Watch[c]; !b {
+				ok = true
+				break
+			}
+		}
 	}
 	return ok
 }
@@ -299,13 +315,21 @@ func (t *recursiveTree) Watch(path string, c chan<- EventInfo, events ...Event) 
 }
 
 // Stop TODO(rjeczalik)
+//
+// TODO(rjeczalik): Split parent watchpoint - transfer watches to children
+// if parent is no longer needed. This carries a risk that underlying
+// watcher calls could fail - reconsider if it's worth the effort.
 func (t *recursiveTree) Stop(c chan<- EventInfo) {
 	var err error
 	fn := func(nd node) (e error) {
 		diff := watchDel(nd, c, all)
 		switch {
-		case diff == none:
+		case diff == none && watchTotal(nd) == 0:
+			// TODO(rjeczalik): There's no watchpoints deeper in the tree,
+			// probably we should remove the nodes as well.
 			return nil
+		case diff == none:
+			// Removing c from nd does not require shrinking its eventset.
 		case diff[1] == 0:
 			if watchIsRecursive(nd) {
 				e = t.w.RecursiveUnwatch(nd.Name)

recursivetree_test.go
@@ -6,7 +6,7 @@ func TestRecursiveTree(t *testing.T) {
 	n := NewRecursiveTreeTest(t, "testdata/vfs.txt")
 	defer n.Close()
 
-	ch := NewChans(4)
+	ch := NewChans(5)
 
 	watches := [...]RCase{
 		// i=0
@@ -262,7 +262,7 @@ func TestRecursiveTree(t *testing.T) {
 			Call: Call{
 				F: FuncWatch,
 				P: "src/github.com/pblaszczyk/qttu",
-				C: ch[0],
+				C: ch[4],
 				E: Write,
 			},
 			Record: []Call{
@@ -380,12 +380,23 @@ func TestRecursiveTree(t *testing.T) {
 				F: FuncStop,
 				C: ch[1],
 			},
+			Record: nil,
+		},
+		// TODO(rjeczalik): research if we can get rid of inactive watchpoints
+		// from "src/github.com/pblaszczyk/qttu/src" (debug tree structure).
+		// Those inactive watchpoints seem too excessive.
+		{
+			Call: Call{
+				F: FuncStop,
+				C: ch[4],
+			},
 			Record: []Call{
 				{
-					F:  FuncRewatch,
-					P:  "src/github.com/rjeczalik/fs",
-					E:  Create | Delete | Move,
-					NE: Create | Move,
+					F:  FuncRecursiveRewatch,
+					P:  "src/github.com/pblaszczyk/qttu",
+					NP: "src/github.com/pblaszczyk/qttu",
+					E:  Create | Delete | Write,
+					NE: Create | Delete,
 				},
 			},
 		},

testing_test.go
@@ -123,13 +123,18 @@ func tmptree(root, list string) (string, error) {
 	return root, nil
 }
 
-// caller TODO(rjeczalik)
-func caller() string {
-	_, file, line, ok := runtime.Caller(3)
+// callern TODO(rjeczalik)
+func callern(n int) string {
+	_, file, line, ok := runtime.Caller(n)
 	if !ok {
 		return "<unknown>"
 	}
 	return filepath.Base(file) + ":" + strconv.Itoa(line)
+}
+
+// caller TODO(rjeczalik)
+func caller() string {
+	return callern(3)
 }
 
 // WCase TODO(rjeczalik)

commit 616975d401a0c8f81a32506451210d7121998322

proper fix for broken traverse skip for Stop()
Removes hack introduced by 524da741.

recursivetree.go
@@ -109,22 +109,10 @@ func watchIsRecursive(nd node) bool {
 	ok := nd.Watch.IsRecursive()
 	// TODO(rjeczalik): add a test for len(wp) != 0 change the condition.
 	if wp := nd.Child[""].Watch; len(wp) != 0 {
-		ok = ok || wp.IsRecursive()
 		// If a watchpoint holds inactive watchpoints, it means it's a parent
 		// one, which is recursive by nature even though it may be not recursive
-		// itself. It currently does not work due to some watchpoints being
-		// registered twice - one for actual watchpoint and one for an inactive
-		// one.
-		//
-		//   ok = true
-		//
-		// TODO(rjeczalik): fix this hack:
-		for c, _ := range wp {
-			if _, b := nd.Watch[c]; !b {
-				ok = true
-				break
-			}
-		}
+		// itself.
+		ok = true
 	}
 	return ok
 }
@@ -210,9 +198,11 @@ func (t *recursiveTree) Watch(path string, c chan<- EventInfo, events ...Event) 
 	//
 	// Look for parent watch which already covers the given path.
 	parent := node{}
-	err = t.root.WalkPath(path, func(nd node, _ bool) error {
+	self := false
+	err = t.root.WalkPath(path, func(nd node, isbase bool) error {
 		if watchTotal(nd) != 0 {
 			parent = nd
+			self = isbase
 			return skip
 		}
 		return nil
@@ -222,7 +212,13 @@ func (t *recursiveTree) Watch(path string, c chan<- EventInfo, events ...Event) 
 		// Parent watch found. Register inactive watchpoint, so we have enough
 		// information to shrink the eventset on eventual Stop.
 		// return t.resetwatchpoint(parent, parent, c, eventset|inactive)
-		switch diff := watchAddInactive(parent, c, eventset); {
+		var diff eventDiff
+		if self {
+			diff = watchAdd(cur, c, eventset)
+		} else {
+			diff = watchAddInactive(parent, c, eventset)
+		}
+		switch {
 		case diff == none:
 			// the parent watchpoint already covers requested subtree with its
 			// eventset
@@ -241,7 +237,9 @@ func (t *recursiveTree) Watch(path string, c chan<- EventInfo, events ...Event) 
 			// TODO(rjeczalik): traverse tree instead; eventually track minimum
 			// subtree root per each chan
 		}
-		watchAdd(cur, c, eventset)
+		if !self {
+			watchAdd(cur, c, eventset)
+		}
 		return nil
 	}
 	// case 2: cur is new parent