a7cde5a8ddcb5b1e603564464d655f589b7e6d30
Paweł Błaszczyk <blaszczykpb@gmail.com>
2015-11-01 10:39:05 +0100

    fen: First version passing tests. #86

--- event_stub.go
+++ event_stub.go
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 // +build !darwin,!linux,!freebsd,!dragonfly,!netbsd,!openbsd,!windows
-// +build !kqueue
+// +build !kqueue,!solaris
 
 package notify
 
--- watcher_test.go
+++ watcher_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-// +build darwin linux freebsd dragonfly netbsd openbsd windows
+// +build darwin linux freebsd dragonfly netbsd openbsd windows solaris
 
 package notify
 
