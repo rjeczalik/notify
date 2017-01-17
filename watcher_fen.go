b8dab33f409f63a90858d0eff0da94ed1a692b66
Pawel Blaszczyk <blaszczykpb@gmail.com>
2017-01-17 22:12:36 +0000

    fen: Fixes for golint.

--- watcher_fen.go
+++ watcher_fen.go
@@ -45,7 +45,7 @@ type watched struct {
 
 // Stop implements trigger.
 func (f *fen) Stop() error {
-	return f.cf.port_alert(f.p)
+	return f.cf.portAlert(f.p)
 }
 
 // Close implements trigger.
@@ -92,7 +92,7 @@ func (f *fen) Watched(n interface{}) (*watched, int64, error) {
 
 // init initializes FEN.
 func (f *fen) Init() (err error) {
-	f.p, err = f.cf.port_create()
+	f.p, err = f.cf.portCreate()
 	return
 }
 
@@ -106,12 +106,12 @@ func fi2fo(fi os.FileInfo, p string) FileObj {
 
 // Unwatch implements trigger.
 func (f *fen) Unwatch(w *watched) error {
-	return f.cf.port_dissociate(f.p, FileObj{Name: w.p})
+	return f.cf.portDissociate(f.p, FileObj{Name: w.p})
 }
 
 // Watch implements trigger.
 func (f *fen) Watch(fi os.FileInfo, w *watched, e int64) error {
-	return f.cf.port_associate(f.p, fi2fo(fi, w.p), int(e))
+	return f.cf.portAssociate(f.p, fi2fo(fi, w.p), int(e))
 }
 
 // Wait implements trigger.
@@ -120,7 +120,7 @@ func (f *fen) Wait() (interface{}, error) {
 		pe  PortEvent
 		err error
 	)
-	err = f.cf.port_get(f.p, &pe)
+	err = f.cf.portGet(f.p, &pe)
 	return pe, err
 }
 
