edfc17bc3805e5696f4a564c26fa7ae288c04072
Evgeny Danienko <6655321@bk.ru>
2019-11-14 19:43:55 +0300

    fix deps

--- tree.go
+++ tree.go
@@ -12,8 +12,11 @@ type tree interface {
 	Close() error
 }
 
-func NewTree() tree {
+func newTree() tree {
 	c := make(chan EventInfo, buffer)
 	w := newWatcher(c)
+	if rw, ok := w.(recursiveWatcher); ok {
+		return newRecursiveTree(rw, c)
+	}
 	return newNonrecursiveTree(w, c, make(chan EventInfo, buffer))
 }
--- watcher_inotify.go
+++ watcher_inotify.go
@@ -51,7 +51,7 @@ type inotify struct {
 }
 
 // NewWatcher creates new non-recursive inotify backed by inotify.
-func newWatcher(c chan<- EventInfo) *inotify {
+func newWatcher(c chan<- EventInfo) watcher {
 	i := &inotify{
 		m:      make(map[int32]*watched),
 		fd:     invalidDescriptor,
