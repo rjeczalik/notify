5cb91165ce76de7b8a6c27057ba261547954ee77
Josh Bleecher Snyder <josharian@gmail.com>
2022-12-06 12:18:50 -0800

    all: gofmt with Go 1.19

--- watcher_recursive_test.go
+++ watcher_recursive_test.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+//go:build (darwin && !kqueue && cgo) || windows
 // +build darwin,!kqueue,cgo windows
 
 package notify
